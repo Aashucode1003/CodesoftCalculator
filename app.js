5666c99d8b61ef60abd5f90911d75856a9cbb984
Aashlesha Shinde <aashleshashinde2002@gmail.com>
2023-07-27 22:44:41 +0530

    FourthCommit

--- app.js
+++ app.js
@@ -3,23 +3,25 @@ let display = document.getElementById('display');
 let buttons = Array.from(document.getElementsByClassName('button'));
 buttons.map( button => {
     button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-            case 'C':
-                display.innerText = '';
-                break;
-            case '=':
-                try{
-                    display.innerText = eval(display.innerText);
-                } catch {
-                    display.innerText = "Error"
-                }
-                break;
-            case '←':
-                if (display.innerText){
-                   display.innerText = display.innerText.slice(0, -1);
-                }
-                break;
-            default:
+        if (e.target.innerText == 'C') {
+
+            display.innerText = '';
+           
+        }
+        else if(e.target.innerText == '='){
+        try {
+            display.innerText = eval(display.innerText);
+        } catch{
+            display.innerText = "Error";
+        }
+       }
+      else if(e.target.innerText == '←'){
+            if (display.innerText) {
+                display.innerText = display.innerText.slice(0, -1);
+            }
+        
+        }
+        else{
                 display.innerText += e.target.innerText;
         }
     });
